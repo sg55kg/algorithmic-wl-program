fbc23232a95d7021d7b6fec447a897633624eb53
Sam Dollar <sdollar1@gmail.com>
2021-10-12 00:23:19 -0500

    notes added to Day class

--- src/main/java/Day.java
+++ src/main/java/Day.java
@@ -1,8 +1,9 @@
 public class Day {
 
-    protected String[] exercises = new String[5];
+    String snatchVariation, cJVariation, squatVariation, upperVariation, accessory;
 
-    public Day() {
-
-    }
+    //Could possibly make this a Week class instead, and have days be an array or Map of 'day' objects. Depending on if it's
+    // 3-6 days a week it would fill the week with the day objects and then distribute the exercises/reps/weight based on
+    // some sort of formula. Like if day 1 is heavy/high volume, day 2 will adjust to be easier. If the week is a deload-type
+    // then the overall volume/weight will be less.
 }
